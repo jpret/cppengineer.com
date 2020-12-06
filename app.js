13a108a2a6ae421c1d2b8881373d64406a5ceb54
jangabrielpretorius <jgdpret@gmail.com>
2020-12-06 20:57:55 +0100

    Added welcome message gif and changed the prod / dev startup to include localhost only on dev env.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const connectDB = require('./middleware/db');
 
 // Connect DB
 connectDB();
- 
+
 // Set view engine - EJS
 app.use(expressLayouts);
 app.set('layout', 'layouts/main.ejs');
@@ -47,13 +47,13 @@ app.use(methodOverride(function (req, res) {
 
 // Express session
 app.use(
-    session({
-      secret: 'my super secret for CppEngineer',
-      resave: true,
-      saveUninitialized: true,
-      store: new MongoStore({ mongooseConnection: mongoose.connection })
-    }) 
-  );
+  session({
+    secret: 'my super secret for CppEngineer',
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
 
 // Passport middleware
 app.use(passport.initialize());
@@ -63,12 +63,12 @@ app.use(passport.session());
 app.use(flash());
 
 // Global variables
-app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
-  });
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Routes
 app.use('/', require('./routes/index'));
@@ -78,8 +78,23 @@ app.use('/posts', require('./routes/posts'));
 // Static folder files
 app.use(express.static('assets'));
 
-// Start server
-app.listen(
+if (process.env.NODE_ENV == 'development') {
+
+  // Start server - development
+  app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+  );
+
+} else if (process.env.NODE_ENV == 'production') {
+
+  // Start server - production
+  app.listen(
+    PORT, 'localhost',
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  );
+
+} else {
+  console.error("No environment specified!");
+  process.exit(1);
+}
