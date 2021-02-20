4eb4ca3c5f2cb16373f4ab28262fe21b0ada695b
jangabriel <jangabriel@cppengineer.com>
2021-02-20 10:46:02 +0100

    Recaptcha added. Now to test on server.

---
+++ middleware/recaptcha.js
@@ -0,0 +1,49 @@
+const https = require('https');
+
+function doVerifyReCapthca (userResponse) {
+    return new Promise((resolve, reject) => {
+        var postData = JSON.stringify({
+            secret: `${process.env.GOOGLE_RECAPTCHA_SECRET_KEY}`,
+            response: userResponse
+        });
+
+        var options = {
+            hostname: 'www.google.com',
+            port: 443,
+            path: '/recaptcha/api/siteverify',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': postData.length
+            }
+        };
+
+        var req = https.request(options, async (res) => {
+            //console.log('statusCode:', res.statusCode);
+            //console.log('headers:', res.headers);
+            let responseBody = '';
+
+            res.on('data', (chunk) => {
+                responseBody += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(JSON.parse(responseBody));
+            });
+        });
+
+        req.on('error', (err) => {
+            reject(err);
+        });
+
+        req.write(postData)
+        req.end();
+    });
+}
+
+module.exports = {
+    verifyReCapthca: async function (userResponse) {
+        // return await the response
+        return await doVerifyReCapthca(userResponse);
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ var moment = require('moment');
 // Imports - Middleware
 const { ensureAuthenticated, forwardAuthenticated } = require('../middleware/auth');
 const { sendEmail } = require('../middleware/email');
+const { doVerifyReCapthca, verifyReCapthca } = require('../middleware/recaptcha');
 // Imports - Models
 const Post = require('./../models/Post');
 const ContactMessage = require('../models/ContactMessage');
@@ -26,14 +27,14 @@ router.get('/', async (req, res) => {
             basePath: "blog",
             helper: require('../helpers/helper')
         });
-  
+
     } catch (error) {
         console.error(error);
         res.render('error/404', {
-          redirect:"/"
-      });
+            redirect: "/"
+        });
     }
-  });
+});
 
 // @desc    About page
 // @route   GET /about
@@ -51,20 +52,40 @@ router.get('/contact', (req, res) => {
 // @route   POST /contact
 router.post('/contact', async (req, res) => {
     try {
-        if (req.body.name.length > 100){
-            req.flash('error_msg', 'That name is too long...');
-            res.render("contact");
-        } else if (req.body.email.length > 100){
-            req.flash('error_msg', 'That email is too long...');
-            res.render("contact");
-        } else if (req.body.body.length > 3500){
-            req.flash('error_msg', 'That message is too long...');
-            res.render("contact");
+        if (req.body.name.length > 100) {
+            res.render("contact", {
+                error_msg: "That name is too long..."
+            });
+        } else if (req.body.email.length > 100) {
+            res.render("contact", {
+                error: "That email is too long..."
+            });
+        } else if (req.body.body.length > 3500) {
+            res.render("contact", {
+                error: "That message is too long..."
+            });
+        } else {
+            // Get the user recaptcha
+            const userRecaptcha = req.body['g-recaptcha-response'];
+            let challengeResult = await verifyReCapthca(userRecaptcha);
+
+            // Verify Recaptcha
+            if (challengeResult.success == false) {
+                res.render("contact", {
+                    error_msg: "Are you a robot?... please try the recaptcha again"
+                });
+            } else if (challengeResult.success == true) {
+                const contactMessage = await ContactMessage.create(req.body);
+                sendEmail(contactMessage);
+                req.flash('success_msg', "Contact Message received successfully! I'll get back to you as soon as possible.");
+                res.redirect('/');
+            } else {
+                console.log("challengeResult: 500");
+                res.render('error/500', {
+                    redirect: "/"
+                });
+            }
         }
-        const contactMessage = await ContactMessage.create(req.body);
-        sendEmail(contactMessage);
-        req.flash('success_msg', "Contact Message received successfully! I'll get back to you as soon as possible.");
-        res.redirect('/');
     } catch (error) {
         console.log(error);
         res.render('error/500', {
@@ -76,24 +97,24 @@ router.post('/contact', async (req, res) => {
 // @desc    Member dashboard page
 // @route   GET /about
 router.get('/dashboard', ensureAuthenticated, async (req, res) => {
-  try {
-    const posts = await Post.find({ user: req.user.id })
-        .populate('user')
-        .sort({ createdAt: 'desc' })
-        .lean()
-    res.render('dashboard', {
-      user: req.user,
-      posts:posts,
-      moment:moment,
-      layout: 'layouts/main_user'
-    });
+    try {
+        const posts = await Post.find({ user: req.user.id })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean()
+        res.render('dashboard', {
+            user: req.user,
+            posts: posts,
+            moment: moment,
+            layout: 'layouts/main_user'
+        });
 
-} catch (error) {
-    console.error(error);
-    res.render('error/500', {
-        redirect:"/"
-    });
-}
+    } catch (error) {
+        console.error(error);
+        res.render('error/500', {
+            redirect: "/"
+        });
+    }
 });
 
 module.exports = router;
